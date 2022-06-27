d80e0a6e0b4d3510a30f30e71e524baafe857c41
Skierhy <Skierhy@gmail.com>
2022-06-27 16:45:13 -0500

    termino de footer php

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -11,6 +11,7 @@
 	<body>
 		<!-- operador ternario para saber si estamos en el index html -->
 		<!-- como las demas archivos php no tiene la variable usamos isset para saber si existe esa variable -->
+		<!-- no tener problemas en produccion IMPORTANTE -->
 		<header class="header <?php echo isset($inicio) ? 'inicio' : '' ?>">
 			<div class="contenedor contenido-header">
 				<div class="barra">
