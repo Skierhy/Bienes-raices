c05554234015479ce2e8b542ef2cf71430b4bbfc
Skierhy <Skierhy@gmail.com>
2022-07-06 21:01:19 -0500

    terminado hasta actualizar

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,22 @@
 <?php
+// mostrar listado de propiedades:
+// importar la conexion
+// base de datos
+require "../includes/config/database.php";
+// conectar base de datos
+$db = conectarBD();
+// escribir el query
+// que es query de seleccion de todos los registros de la tabla propiedades
+// query para obtener todos los registros de la tabla propiedades
+// selecionamos todo
+// de la tabla propiedades
+$query = "SELECT * FROM propiedades";
+
+// consultar Base de datos
+
+$resultadoConsulta = mysqli_query($db, $query);
+
+
 
 // echo '<pre>';
 // var_dump($_GET);
@@ -21,6 +39,10 @@ incluir_Template('header');
 	?>
 		<p class="alerta exito">Anuncio Creado Correctamente</p>
 	<?php
+	elseif ($resultado == 2) :
+	?>
+		<p class="alerta exito">Anuncio Actualizado</p>
+	<?php
 	endif;
 	?>
 	<a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
@@ -35,21 +57,51 @@ incluir_Template('header');
 				<th>Acciones</th>
 			</tr>
 		</thead>
-
+		<!-- mostrar resultados -->
 		<tbody>
-			<tr>
-				<th>1</th>
-				<th>Casa en la playa</th>
-				<th> <img src="/imagenes/86e004403e891b6e659e25d2711625de.jpg" alt="imagen propiedad" class="imagen-tabla"> </th>
-				<th>$120000000</th>
-				<th>
-					<a href="#" class="boton-rojo-block">Eliminar</a>
-					<a href="#" class="boton-amarillo-block">Actualizar</a>
-				</th>
-			</tr>
+			<?php
+			// recorrer resultados
+			// mysqli_fetch_assoc = devuelve un array asociativo con los datos de la fila actual
+
+			while ($propiedad = mysqli_fetch_assoc($resultadoConsulta)) :
+			?>
+				<tr>
+					<th>
+						<?php
+						echo $propiedad['id'];
+						?>
+					</th>
+					<th>
+						<?php
+						echo $propiedad['titulo'];
+						?>
+					</th>
+					<th> <img src="/imagenes/<?php echo $propiedad['imagen'] ?>" alt="imagen propiedad" class="imagen-tabla"> </th>
+					<th>
+						$
+						<?php
+						echo $propiedad['precio'];
+						?>
+					</th>
+					<th>
+						<a href="#" class="boton-rojo-block">Eliminar</a>
+						<a href="/admin/propiedades/actualizar.php?id=<?php echo $propiedad['id'] ?>" class="boton-amarillo-block">
+							Actualizar
+						</a>
+					</th>
+				</tr>
+			<?php
+			endwhile;
+			?>
 		</tbody>
 	</table>
 </main>
 <?php
+// cerrar conexion
+// por que se debe que cerrar la conexion
+// para que no se quede abierta
+// si no se cierra, se queda abierta y no se puede usar
+// la base de datos
+mysqli_close($db);
 incluir_Template('footer');
 ?>
--- includes/templates/header.php
+++ includes/templates/header.php
@@ -1,48 +1,42 @@
 <!DOCTYPE html>
 <html lang="es">
-	<head>
-		<meta charset="UTF-8" />
-		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<title>Bienes raíces</title>
-		<!-- la / sirve para que en la raiz de la carpeta main hay una carpeta que se llama build -->
-		<!-- en de usar ../ -->
-		<link rel="stylesheet" href="/build/css/app.css" />
-	</head>
 
-	<body>
-		<!-- operador ternario para saber si estamos en el index html -->
-		<!-- como las demas archivos php no tiene la variable usamos isset para saber si existe esa variable -->
-		<!-- no tener problemas en produccion IMPORTANTE -->
-		<!-- ahorita ya no es necesario por que funciones.php esta por defecto el parametro -->
-		<header class="header <?php echo $inicio ? 'inicio' : '' ?>">
-			<div class="contenedor contenido-header">
-				<div class="barra">
-					<!-- para referir a la pagina principal -->
-					<a href="index.php">
-						<img
-							src="/build/img/logo.svg"
-							alt="Logotipo de Bienes raíces"
-						/>
-					</a>
-					<div class="mobile-menu">
-						<img src="/build/img/barras.svg" alt="icon menu responsive" />
-					</div>
-					<div class="derecha">
-						<img
-							src="/build/img/dark-mode.svg"
-							alt="dark mode"
-							class="dark-mode-boton"
-						/>
-						<nav class="navegacion mostrar">
-							<a href="nosotros.php">Nosotros</a>
-							<a href="anuncios.php">Anuncios</a>
-							<a href="blog.php">Blog</a>
-							<a href="contacto.php">Contacto</a>
-						</nav>
-					</div>
+<head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Bienes raíces</title>
+	<!-- la / sirve para que en la raiz de la carpeta main hay una carpeta que se llama build -->
+	<!-- en de usar ../ -->
+	<link rel="stylesheet" href="/build/css/app.css" />
+</head>
+
+<body>
+	<!-- operador ternario para saber si estamos en el index html -->
+	<!-- como las demas archivos php no tiene la variable usamos isset para saber si existe esa variable -->
+	<!-- no tener problemas en produccion IMPORTANTE -->
+	<!-- ahorita ya no es necesario por que funciones.php esta por defecto el parametro -->
+	<header class="header <?php echo $inicio ? 'inicio' : '' ?>">
+		<div class="contenedor contenido-header">
+			<div class="barra">
+				<!-- para referir a la pagina principal -->
+				<a href="/index.php">
+					<img src="/build/img/logo.svg" alt="Logotipo de Bienes raíces" />
+				</a>
+				<div class="mobile-menu">
+					<img src="/build/img/barras.svg" alt="icon menu responsive" />
 				</div>
-				<!-- .barra -->
+				<div class="derecha">
+					<img src="/build/img/dark-mode.svg" alt="dark mode" class="dark-mode-boton" />
+					<nav class="navegacion mostrar">
+						<a href="nosotros.php">Nosotros</a>
+						<a href="anuncios.php">Anuncios</a>
+						<a href="blog.php">Blog</a>
+						<a href="contacto.php">Contacto</a>
+					</nav>
+				</div>
 			</div>
-			<!-- .contenedor contenido -->
-		</header>
+			<!-- .barra -->
+		</div>
+		<!-- .contenedor contenido -->
+	</header>
